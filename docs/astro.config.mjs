2454d6eb321fc0486e72db0c9775a944466ae9d2
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-07-01 16:40:17 +0200

    docs: use starlight for docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), svelte(), tailwind()],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
+});
