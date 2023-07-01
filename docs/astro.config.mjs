92ff0a6879dc9601ac00a933dcda48f6101aefc7
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-07-01 23:51:53 +0200

    feat: use astro for docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,35 +1,45 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        link: '/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), svelte(), tailwind()],
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  integrations: [
+    starlight({
+      title: "smu",
+      defaultLocale: "root",
+      locales: {
+        root: {
+          lang: "en",
+          label: "English",
+        },
+      },
+      social: {
+        github: "https://github.com/dmnkgrc/smu",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    svelte(),
+    tailwind(),
+  ],
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  }
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
 });
