c9d251e707ec9db43055031cfb4cd77f0333b8c9
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-07-21 20:17:22 +0200

    chore: remove website from repo

--- website/src/lib/NavLink.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let href: string;
-	export let target: string | undefined = undefined;
-	export let rel: string | undefined = undefined;
-</script>
-
-<a {href} {target} {rel} class="btn btn-sm variant-ghost-surface flex items-center gap-3"
-	><slot /></a
->
